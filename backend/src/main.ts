540328430b0e6811dc5b1995cd21a059854c413e
suraj171299 <surajpadalkar1999@gmail.com>
2023-12-12 13:27:27 +0530

    ValidationErrorHandling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,6 @@ import { ValidationMiddleware } from './middleware/validation.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(new ValidationMiddleware());
   await app.listen(3000);
 }
 bootstrap();
